be1012dbccbf64a938f3b5a4e68e79b99fcf5149
Darion <166887207+dariont5@users.noreply.github.com>
2025-08-29 17:58:44 -0500

    Solve: solve 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    }
+    if (num1 % 1 !== 0 || num2 % 1 !== 0) {
+        return 'ERROR';
+    }
+    if (typeof(num1) !== 'number' || typeof(num2) !== 'number' ){
+        return 'ERROR';
+    }
+    let left = Math.min(num1, num2);
+    let right = Math.max(num1, num2);
+    let sum = 0;
+    for (let i = left; i <= right; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
