fd7f75cb649d31269c8d61e99c29dbccdbe02336
Darion <166887207+dariont5@users.noreply.github.com>
2025-08-29 17:45:36 -0500

    Solve: solve 08

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+/*
+Loop through Args:
+    If Array includes Arg:
+        Remove all from array
+Return array
+*/
+const removeFromArray = function(array, ...args) {
+    let newArray = array
+    for (arg of args) {
+        newArray = newArray.filter(item => item !== arg);
+    }
+    return newArray
 };
 
 // Do not edit below this line
